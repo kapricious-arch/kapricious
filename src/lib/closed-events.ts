6138bdd052cbf2e4e1eec0a7d9d5717ee463fc6a
Adhil <adhilsalam200@gmail.com>
2026-03-28 11:16:25 +0530

    Events Closing Worst Code

--- src/lib/closed-events.ts
+++ src/lib/closed-events.ts
@@ -1,7 +1,9 @@
 export const CLOSED_EVENT_IDS = new Set([
+  "bgmi-tournament",
   "bridgit",
   "build-a-pc",
   "bug-bounty",
+  "code-catastrophe",
   "css-royale",
   "prompt-wars",
   "tech-escape-room",
